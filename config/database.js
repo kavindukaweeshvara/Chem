5bb2daeb5020df4f20a76d2dece2508d047f4ce6
Silentbot <101484992+kavindukaweeshvara@users.noreply.github.com>
2026-07-14 22:29:03 +0530

    Create database.js

---
+++ config/database.js
@@ -0,0 +1,22 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+module.exports = { sequelize, Sequelize };
